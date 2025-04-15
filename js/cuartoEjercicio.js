863fea18f538fd64574e200495dd1ba1e0c6fd5f
Luchiano <elvitor2130@gmail.com>
2025-04-15 19:08:51 -0300

    realizado

---
+++ js/cuartoEjercicio.js
@@ -0,0 +1,5 @@
+//escribe un programa de dos lineas que pida el nombre del usuario con un prompt y escriba un texto que diga <hola nombreUsuario>
+
+let nombreUsuario = prompt('Ingresa tu nombre de usuario:')
+
+document.writeln('¡Hola '+ nombreUsuario +'!')

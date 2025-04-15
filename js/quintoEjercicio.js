90d3a4ed4fb2aed585b6f6bc5267211c058c47a3
Luchiano <elvitor2130@gmail.com>
2025-04-15 19:16:40 -0300

    realizado

---
+++ js/quintoEjercicio.js
@@ -0,0 +1,7 @@
+//escribe un programa de tres lineas que pida un numero, pida otro numero y escriba el resultado de sumar estos dos numeros. tener en cuenta la siguiente funcion: parseint.
+
+let num1 = parseInt(prompt('Ingrese un número'))
+
+let num2 = parseInt(prompt('Ingrese un segundo número'))
+
+document.writeln('La suma entre estos dos números es: '+ (num1 + num2))

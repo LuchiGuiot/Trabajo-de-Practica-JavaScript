c15029ea6a90c59421ddc647be925a60ac348cc8
Luchiano <elvitor2130@gmail.com>
2025-04-15 22:30:16 -0300

    agregué la consigna y el numero a colocar

---
+++ 10-decimoEjercicio.js
@@ -0,0 +1,3 @@
+//escribe un programa que pida un numero y nos diga si es divisible en 2,3,5 o 7 (solo hay que comprobar si lo es por uno de los cuatro)
+
+const num = parseInt(prompt('Ingrese un número para saber si es divisible en 2, 3, 5 o 7.'))
